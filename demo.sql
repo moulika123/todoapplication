2d40199e636e4cab971cc0cd9cf5d1bf239e5a54
moulika123 <79619123+moulika123@users.noreply.github.com>
2021-03-11 23:35:12 +0530

    Add files via upload

---
+++ demo.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.0.67-community-nt : Database - demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`demo` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `demo`;
+
+/*Table structure for table `todos` */
+
+DROP TABLE IF EXISTS `todos`;
+
+CREATE TABLE `todos` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `description` varchar(250) NOT NULL,
+  `is_done` bit(1) default NULL,
+  `target_date` varchar(22) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `todos` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL auto_increment,
+  `first_name` varchar(20) default NULL,
+  `last_name` varchar(20) default NULL,
+  `username` varchar(250) default NULL,
+  `password` varchar(20) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`first_name`,`last_name`,`username`,`password`) values (1,'sss','sss','sss','sss'),(2,'mmm','hhh','mmhh','mmhh');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
